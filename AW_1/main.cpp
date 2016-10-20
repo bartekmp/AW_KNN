a5f005810087c398960a4ebab3c7f6707e569cae
bartekmp <bartekmp@outlook.com>
2016-10-21 00:04:48 +0200

    speedup 2

--- AW_1/main.cpp
+++ AW_1/main.cpp
@@ -5,29 +5,35 @@
 #include <map>
 #include <algorithm>
 #include <iterator>
+#include "omptl/omptl_algorithm"
 //#include <Windows.h>
 
 struct Point
 {
-	int x, y, type;
+	int *xi;
+	unsigned int type, dimension;
 	double dist;
-	Point(int _x, int _y, int _t, double _d = 0.0): x(_x), y(_y), type(_t), dist(_d)
+	Point(int *_x, int _d, int _t, double _dist = 0.0): xi(_x), type(_t), dimension(_d), dist(_dist)
 	{}
+	Point(int _d, int _t, double _dist = 0.0) : type(_t), dimension(_d), dist(_dist)
+	{
+		xi = new int[dimension];
+	}
 	Point()
 	{
-		x = 0; y = 0; type = 0; dist = 0.0;
+		dimension = 2; xi = new int[dimension]; type = 0; dist = 0.0;
 	}
-
+	~Point()
+	{
+		delete[] xi;
+	}
 	void distance(Point& other)
 	{
-		dist = sqrt(pow(other.x - this->x, 2) + pow(other.y - this->y, 2));
-	}
-
-	std::string to_string() const
-	{
-		std::stringstream ss;
-		ss << "x: " << x << ", y: " << y << ", type: " << type;
-		return ss.str();
+		for(size_t i = 0; i < dimension; ++i)
+		{
+			dist += pow(other.xi[i] - this->xi[i], 2);
+		}
+		dist = sqrt(dist);
 	}
 
 	bool operator< (const Point& other) const
@@ -40,15 +46,17 @@ struct Point
 unsigned int max_element(std::map<unsigned int, unsigned int> &container)
 {
 	std::map<unsigned int, unsigned int>::const_iterator it = container.begin();
-	unsigned int temp = it->second;
+	unsigned int temp = it->second, temp_key = it->first;
+
 	for (unsigned int i = 1; i < container.size(); ++i)
 	{
 		if ((++it)->second > temp)
 		{
 			temp = it->second;
+			temp_key = it->first;
 		}
 	}
-	return temp;
+	return temp_key;
 }
 
 /*int cmpfunc(const void* _a, const void* _b)
@@ -62,23 +70,34 @@ unsigned int max_element(std::map<unsigned int, unsigned int> &container)
 	return -1;
 }*/
 
-void initialize_learning_set(Point *learning_vector, unsigned int learning_set_count)
+void initialize_learning_set(Point *learning_vector, unsigned int learning_set_count, unsigned int dim)
 {
 	for (unsigned int i = 0; i < learning_set_count; ++i)
 	{
-		int x, y, t;
-		std::cin >> x >> y >> t;
-		learning_vector[i] = Point(x, y, t);
+		int xi, t;
+		std::cin >> t;
+		Point temp = Point(dim, t);
+		for(unsigned int j = 0; j < dim; ++j)
+		{
+			std::cin >> xi;
+			temp.xi[j] = xi;
+		}
+		learning_vector[i] = temp;
 	}
 }
 
-void initialize_test_set(Point *test_vector, unsigned int test_set_count)
+void initialize_test_set(Point *test_vector, unsigned int test_set_count, unsigned int dim)
 {
 	for (unsigned int i = 0; i < test_set_count; ++i)
 	{
-		int x, y;
-		std::cin >> x >> y;
-		test_vector[i] = Point(x, y, 0);
+		int xi;
+		Point temp = Point(dim, 0);
+		for (unsigned int j = 0; j < dim; ++j)
+		{
+			std::cin >> xi;
+			temp.xi[j] = xi;
+		}
+		test_vector[i] = temp;
 	}
 }
 
@@ -90,7 +109,8 @@ bool compare_points(Point a, Point b)
 void classify(Point &test_point, Point *learning_set, unsigned int learning_set_count)
 {
 	//std::pair<Point, double> *distances = new std::pair<Point, double>[learning_set_count];
-	for(unsigned int it = 0; it < learning_set_count; ++it)
+#pragma omp parallel for shared(learning_set)
+	for(int it = 0; it < learning_set_count; ++it)
 	{
 		learning_set[it].distance(test_point);
 		//distances[it] = { learning_set[it], d };
@@ -107,7 +127,7 @@ int main()
 	omp_init_lock(&lock);
 
 
-	Point *learning_set, *test_set, *learning_set_for_thread;
+	Point *learning_set, *test_set;
 	
 	/*LARGE_INTEGER frequency;
 	if (::QueryPerformanceFrequency(&frequency) == FALSE)
@@ -119,17 +139,18 @@ int main()
 	std::cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
 
-	unsigned int K, learning_set_count, test_set_count;
+	unsigned int K, learning_set_count, test_set_count, dimensions;
 	std::cin >> K;
+	std::cin >> dimensions;
 	std::cin >> learning_set_count;
 
 	learning_set = new Point[learning_set_count];
-	initialize_learning_set(learning_set, learning_set_count);
+	initialize_learning_set(learning_set, learning_set_count, dimensions);
 	
 	std::cin >> test_set_count;
 
 	test_set = new Point[test_set_count];
-	initialize_test_set(test_set, test_set_count);	
+	initialize_test_set(test_set, test_set_count, dimensions);	
 	/*
 	LARGE_INTEGER start;
 	LARGE_INTEGER end;
@@ -137,27 +158,31 @@ int main()
 		throw "foo";
 		*/
 
-#pragma omp parallel for private(learning_set_for_thread) shared(learning_set)
+//#pragma omp parallel for shared(learning_set) num_threads(2)
 		for (int it = 0; it < test_set_count; ++it)
 		{
+			//Point *learning_set_for_thread;
 			Point *i = &test_set[it];
-			learning_set_for_thread = new Point[learning_set_count];
-			std::memcpy(learning_set_for_thread, learning_set, sizeof(Point));
-			//std::copy(std::begin(learning_set), std::end(learning_set), std::begin(learning_set_for_thread));
-			classify(*i, learning_set_for_thread, learning_set_count);
+			//learning_set_for_thread = new Point[learning_set_count];
+			//std::memcpy(learning_set_for_thread, learning_set, sizeof(Point));
+			
+			////std::copy(std::begin(learning_set), std::end(learning_set), std::begin(learning_set_for_thread));
+			classify(*i, learning_set/*_for_thread*/, learning_set_count);
 
 			std::map<unsigned int, unsigned int> labels;
 
 			for (int n = 0; n < K; ++n)
 			{
-				labels[learning_set_for_thread[n].type];
-				labels[learning_set_for_thread[n].type]++;
+				labels[learning_set/*_for_thread*/[n].type];
+				labels[learning_set/*_for_thread*/[n].type]++;
 			}
 			unsigned int type = max_element(labels);
 
 			(*i).type = type;
-			delete[] learning_set_for_thread;
-			//omp_set_lock(&lock); 		
+			//delete[] learning_set_for_thread;
+
+			//omp_set_lock(&lock); 	
+			//std::cout << "point type " << type << std::endl;
 			//omp_unset_lock(&lock);
 		}
 	/*
@@ -168,5 +193,6 @@ int main()
 	*/
 	delete[] learning_set;
 	delete[] test_set;
+	omp_destroy_lock(&lock);
 	return 0;
 }
--- AW_1/test_data.py
+++ AW_1/test_data.py
@@ -1,33 +1,45 @@
 import random
 import sys
 K = 4 if len(sys.argv) <= 1 else int(sys.argv[1])
-learning_cnt = 10 if len(sys.argv) <= 2 else int(sys.argv[2])
+dim = 2 if len(sys.argv) <= 2 else int(sys.argv[2])
+learning_cnt = 10 if len(sys.argv) <= 3 else int(sys.argv[3])
 learning_data = []
 for i in range(learning_cnt):
-    x, y = random.randint(-10000, 10000), random.randint(-10000, 10000)
-    if x >= 0 and y >= 0:
+    xi = []
+    for j in range(dim):
+        xi.append(random.randint(-10000, 10000))
+    if xi[0] >= 0 and xi[1] >= 0:
         c = '1'
-    if x < 0 and y >= 0:
+    if xi[0] < 0 and xi[1] >= 0:
         c = '2'
-    if x < 0 and y < 0:
+    if xi[0] < 0 and xi[1] < 0:
         c = '3'
-    if x >= 0 and y < 0:
+    if xi[0] >= 0 and xi[1] < 0:
         c = '4'
-    learning_data.append((c, x, y))
+    learning_data.append((c, xi))
     
-test_cnt = 2 if len(sys.argv) <= 3 else int(sys.argv[3])
+test_cnt = 2 if len(sys.argv) <= 4 else int(sys.argv[4])
 test_data = []
 for i in range(test_cnt):
-    x, y = random.randint(-10000, 10000), random.randint(-10000, 10000)
-    test_data.append((x, y))
+    xj = []
+    for j in range(dim):
+        xj.append(random.randint(-10000, 10000))
+    test_data.append((xj))
 
 with open('data', 'w') as f:
     f.write(str(K)+'\n')
+    f.write(str(dim)+'\n')
     f.write(str(learning_cnt)+'\n')
     for i in learning_data:
-        f.write(str(i[1]) + ' ' + str(i[2]) + ' ' + i[0] + '\n')
+        line = ''
+        for j in i[1]:
+            line += ' ' + str(j)
+        f.write(i[0] + line + '\n')
     f.write(str(test_cnt)+'\n')
     for i in test_data:
-        f.write(str(i[0]) + ' ' + str(i[1]) + '\n')
+        line = ''
+        for j in i:
+            line += str(j) + ' '
+        f.write(line + '\n')
 
         
