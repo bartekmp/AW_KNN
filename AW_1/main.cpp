c227eb6bd88e8437672ee48f20afeb20f900bbf4
bartekmp <bartekmp@outlook.com>
2016-10-27 00:01:19 +0200

    changed memory allocation to aligned, changed dimensions to 3 and some other changes to Point struct

--- AW_1/main.cpp
+++ AW_1/main.cpp
@@ -2,43 +2,57 @@
 #include <omp.h>
 #include <sstream>
 #include <cmath>
+#include <cstring>
 #include <map>
 #include <algorithm>
 #include <iterator>
-#include "omptl/omptl_algorithm"
+#include <vector>
 //#include <Windows.h>
+
+#define DEBUG
+#undef DEBUG
+omp_lock_t lock;
 
 struct Point
 {
-	int *xi;
+	int x, y, z;
 	unsigned int type, dimension;
 	double dist;
-	Point(int *_x, int _d, int _t, double _dist = 0.0): xi(_x), type(_t), dimension(_d), dist(_dist)
+	Point(int _x, int _y, int _z, int _d, int _t, double _dist = 0.0): x(_x), y(_y), z(_z), type(_t), dimension(_d), dist(_dist)
 	{}
-	Point(int _d, int _t, double _dist = 0.0) : type(_t), dimension(_d), dist(_dist)
+	Point(int _d, int _t) : type(_t), dimension(_d)
 	{
-		xi = new int[dimension];
+		x = y = z = 0;
+		dist = 0.0;
 	}
 	Point()
 	{
-		dimension = 2; xi = new int[dimension]; type = 0; dist = 0.0;
+		dimension = 2; x = y = z = 0; type = 0; dist = 0.0;
 	}
-	~Point()
+	Point(const Point &obj)
 	{
-		delete[] xi;
+		this->type = obj.type;
+		this->dimension = obj.dimension;
+		this->x = obj.x;
+		this->y = obj.y;
+		this->z = obj.z;
+		this->dist = obj.dist;
 	}
 	void distance(Point& other)
 	{
-		for(size_t i = 0; i < dimension; ++i)
-		{
-			dist += pow(other.xi[i] - this->xi[i], 2);
-		}
-		dist = sqrt(dist);
+		dist = sqrt(pow(other.x - this->x, 2)+ pow(other.x - this->x, 2)+ pow(other.x - this->x, 2));
 	}
 
 	bool operator< (const Point& other) const
 	{
-		return (dist < other.dist);
+		return dist < other.dist;
+	}
+
+	std::string str() const
+	{
+		std::stringstream ss;
+		ss << "x = " << x << ", y = " << y << ", z = " << z << ", type = " << type;
+		return ss.str();
 	}
 };
 
@@ -59,29 +73,17 @@ unsigned int max_element(std::map<unsigned int, unsigned int> &container)
 	return temp_key;
 }
 
-/*int cmpfunc(const void* _a, const void* _b)
-{
-	const Point *a = (Point*) _a;
-	const Point *b = (Point*) _b;
-	if (a->dist == b->dist)
-		return 0;
-	if (a->dist > b->dist)
-		return 1;
-	return -1;
-}*/
-
 void initialize_learning_set(Point *learning_vector, unsigned int learning_set_count, unsigned int dim)
 {
 	for (unsigned int i = 0; i < learning_set_count; ++i)
 	{
-		int xi, t;
+		int x, y, z, t;
 		std::cin >> t;
 		Point temp = Point(dim, t);
-		for(unsigned int j = 0; j < dim; ++j)
-		{
-			std::cin >> xi;
-			temp.xi[j] = xi;
-		}
+
+		std::cin >> x >> y >> z;
+		temp.x = x; temp.y = y; temp.z = z;
+
 		learning_vector[i] = temp;
 	}
 }
@@ -90,51 +92,46 @@ void initialize_test_set(Point *test_vector, unsigned int test_set_count, unsign
 {
 	for (unsigned int i = 0; i < test_set_count; ++i)
 	{
-		int xi;
+		int x, y, z;
 		Point temp = Point(dim, 0);
-		for (unsigned int j = 0; j < dim; ++j)
-		{
-			std::cin >> xi;
-			temp.xi[j] = xi;
-		}
+		std::cin >> x >> y >> z;
+		temp.x = x; temp.y = y; temp.z = z;
+
 		test_vector[i] = temp;
 	}
 }
 
-bool compare_points(Point a, Point b)
-{
-	return (a.dist < b.dist);
-}
-
 void classify(Point &test_point, Point *learning_set, unsigned int learning_set_count)
 {
-	//std::pair<Point, double> *distances = new std::pair<Point, double>[learning_set_count];
-#pragma omp parallel for shared(learning_set)
 	for(int it = 0; it < learning_set_count; ++it)
 	{
 		learning_set[it].distance(test_point);
-		//distances[it] = { learning_set[it], d };
 	}
 	std::sort(learning_set, learning_set + learning_set_count);
-	//qsort(learning_set, learning_set_count, sizeof(Point), cmpfunc);
-	//return distances;
+
+#ifdef DEBUG
+	omp_set_lock(&lock);
+	std::cout << "distance from: " << test_point.str() << std::endl;
+	for (int it = 0; it < learning_set_count; ++it)
+	{
+		std::cout << "to: " << learning_set[it].str() << " is: " << learning_set[it].dist << std::endl;
+	}
+	omp_unset_lock(&lock);
+#endif
 }
 
 
 int main()
 {
-	omp_lock_t lock;
 	omp_init_lock(&lock);
 
-
-	Point *learning_set, *test_set;
+	Point *learning_set, *test_set, *learning_set_for_thread;
 	
 	/*LARGE_INTEGER frequency;
 	if (::QueryPerformanceFrequency(&frequency) == FALSE)
-		throw "foo";
-		*/
-
-
+		throw "foo";		
+	*/
+	
 
 	std::cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
@@ -144,12 +141,12 @@ int main()
 	std::cin >> dimensions;
 	std::cin >> learning_set_count;
 
-	learning_set = new Point[learning_set_count];
+	learning_set = static_cast<Point*>(_mm_malloc(learning_set_count*sizeof(Point), 64));
 	initialize_learning_set(learning_set, learning_set_count, dimensions);
 	
 	std::cin >> test_set_count;
 
-	test_set = new Point[test_set_count];
+	test_set = static_cast<Point*>(_mm_malloc(test_set_count * sizeof(Point), 64));
 	initialize_test_set(test_set, test_set_count, dimensions);	
 	/*
 	LARGE_INTEGER start;
@@ -157,42 +154,47 @@ int main()
 	if (::QueryPerformanceCounter(&start) == FALSE)
 		throw "foo";
 		*/
-
-//#pragma omp parallel for shared(learning_set) num_threads(2)
-		for (int it = 0; it < test_set_count; ++it)
-		{
-			//Point *learning_set_for_thread;
-			Point *i = &test_set[it];
-			//learning_set_for_thread = new Point[learning_set_count];
-			//std::memcpy(learning_set_for_thread, learning_set, sizeof(Point));
+#pragma omp parallel for shared(learning_set) private(learning_set_for_thread)
+	for (int it = 0; it < test_set_count; ++it)
+	{
+		Point *i = &test_set[it];
+		learning_set_for_thread = static_cast<Point*>(_mm_malloc(learning_set_count * sizeof(Point), 64));
 			
-			////std::copy(std::begin(learning_set), std::end(learning_set), std::begin(learning_set_for_thread));
-			classify(*i, learning_set/*_for_thread*/, learning_set_count);
+		memcpy(learning_set_for_thread, learning_set, learning_set_count*sizeof(Point));
+			
+		classify(*i, learning_set_for_thread, learning_set_count);
 
-			std::map<unsigned int, unsigned int> labels;
+		std::map<unsigned int, unsigned int> labels;
 
-			for (int n = 0; n < K; ++n)
-			{
-				labels[learning_set/*_for_thread*/[n].type];
-				labels[learning_set/*_for_thread*/[n].type]++;
-			}
-			unsigned int type = max_element(labels);
-
-			(*i).type = type;
-			//delete[] learning_set_for_thread;
-
-			//omp_set_lock(&lock); 	
-			//std::cout << "point type " << type << std::endl;
-			//omp_unset_lock(&lock);
+		for (int n = 0; n < K; ++n)
+		{
+			labels[learning_set_for_thread[n].type];
+			labels[learning_set_for_thread[n].type]++;
 		}
+		unsigned int type;
+		if (learning_set_for_thread[0].dist == 0)
+			type = learning_set_for_thread[0].type;
+		else
+			type = max_element(labels);
+
+		(*i).type = type;
+		_mm_free(learning_set_for_thread);
+#ifdef DEBUG
+		omp_set_lock(&lock); 	
+		std::cout << i->str() << std::endl << std::endl;
+		omp_unset_lock(&lock);
+#endif
+	}
 	/*
 	if (::QueryPerformanceCounter(&end) == FALSE)
 		throw "foo";
 	double interval = static_cast<double>(end.QuadPart - start.QuadPart) / frequency.QuadPart;
 	std::cout << interval << std::endl;
 	*/
-	delete[] learning_set;
-	delete[] test_set;
+
+	_mm_free(learning_set);
+	_mm_free(test_set);
+
 	omp_destroy_lock(&lock);
 	return 0;
 }
